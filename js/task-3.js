30a64ccea7ef91bb76d2a894f6321e410298d114
vadimdushak <98094490+vadimdushak@users.noreply.github.com>
2026-09-07 20:44:34 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+
+    const profile = {
+       username: "Jacob",
+        playTime: 300,
+         getInfo() {
+            return `${this.username} has ${this.playTime} active hours!`;
+    },
+        changeUsername(name) { 
+            this.username = name;
+        },
+        updatePlayTime(time){
+          this.playTime = this.playTime + time;
+
+        }
+
+    }
+   
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
